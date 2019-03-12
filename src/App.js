c4be6dbfe159c4919e692d6efd4d0769b73a4256
capyangyang <capyangyang>
2019-03-13 07:45:35 +0800

    ip upadte

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import './css/flex.css';
-import { Button, Row, Col, Radio, Input } from "antd";
+import { Button, Row, Col, Radio, Input, Form } from "antd";
 const Search = Input.Search;
-let item1Timer;
+const FormItem = Form.Item;
 class App extends Component {
 
   state = {
-    topList: [{ name: "首页", isSelect: true }
-      , { name: "项目检索" }
-      , { name: "成果检索" }
-      , { name: "标准评价" }
-      , { name: "科技评估" }
-      , { name: "项目咨询" }
-      , { name: "奖励咨询" }
-      , { name: "成果交易" }
-      , { name: "联系我们" }
+    search_content: "",
+    topList: [{ name: "首页", isSelect: true, url: " " }
+      , { name: "项目检索", url: "http://125.46.34.231:8000/projectSearch/index" }
+      , { name: "成果检索", url: " " }
+      , { name: "标准评价", url: " " }
+      , { name: "科技评估", url: " " }
+      , { name: "项目咨询", url: " " }
+      , { name: "奖励咨询", url: " " }
+      , { name: "成果交易", url: " " }
+      , { name: "联系我们", url: " " }
       , { name: "登录" }],
     mainList1: [{ name: "ABOUT</br>知耕科技服务平台", isSelect: true, content: "&nbsp&nbsp&nbsp&nbsp知耕科技服务平台是一个面向众人的科技服务平台，致力于为您提供关于科技项目的服务于检索。</br>&nbsp&nbsp&nbsp&nbsp我们能为您定制公平价格，为您提供快速的查询平台，鼓励创新，创造发展。" }
       , { name: "奖励咨询", content: "		&nbsp&nbsp&nbsp&nbsp只有你想不到，没有我们做不到，为了鼓励科研创新精神我们制作了一系列的奖励资讯。</br>&nbsp&nbsp&nbsp&nbsp该资讯能激励你争创佳绩，你的努力，就是我们的动力。", isMove: false }
@@ -28,34 +29,39 @@ class App extends Component {
       , { name: "奖励咨询", content: "奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询奖励咨询", }
     ],
   }
-  constructor(props) {
-    super(props);
-
-  }
 
   render() {
+    let search_content = this.state.search_content;
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className="App am-flexbox-dir-column am-flexbox-align-center ">
         <div className="top">欢迎来到知耕科技服务平台！</div>
         <Row className="top2" type='flex' align='middle' >
           <Col span={10} offset={2}>
-            <img src={require("./img/logo.png")} />
+            <img alt="alt" src={require("./img/logo.png")} />
           </Col>
           <Col span={7}>
-            <img src={require("./img/lx.png")} />
+            <div>
+              <img alt="alt" src={require("./img/top_tel.png")} />
+              <img alt="alt" src={require("./img/top_mail.png")} />
+              <div className='am-flexbox-dir-row'>
+
+
+              </div>
+            </div>
           </Col>
-          <Col span={2}>
+          <Col span={5}>
             <Button type="primary" >登录</Button>
-          </Col>
-          <Col span={3}>
-            <Button >注册</Button>
+            <Button style={{ marginLeft: 20 }} >注册</Button>
           </Col>
         </Row>
 
         <div className="nav">
           <ul>
             {this.state.topList.map((data, index) => (
-              <li key={index} className={data.isSelect ? "tabactive" : ""}  >{data.name}</li>
+              <li key={index} className={data.isSelect ? "tabactive" : ""}  >
+                <a href={data.url}> {data.name} </a>
+              </li>
             ))}
           </ul>
         </div>
@@ -65,31 +71,35 @@ class App extends Component {
           <Col span={15} offset={0}>
 
             <div className='search_left'>
-              <Radio.Group defaultValue="a" size="large" buttonStyle="solid">
+              <Radio.Group onChange={(event) => { console.log(event); }} defaultValue="a" size="large" buttonStyle="solid">
                 <Radio.Button value="a">项目搜索</Radio.Button>
                 <Radio.Button value="b">成果搜索</Radio.Button>
               </Radio.Group>
             </div>
-
             <div className="search_l_b">
-              <Search
-                placeholder="请输入您要检索的内容"
-                enterButton="检索"
-                size="large"
-                onSearch={value => console.log(value)}
-              />
+              <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                  {getFieldDecorator('userName', {
+                    rules: [{ required: true, message: 'Please input your username!' }],
+                  })(
+                    <Input placeholder="Username" />
+                  )}
+                </FormItem>
+              </Form>
             </div>
           </Col>
-          <Col span={6} offset={1}>
-            <div>
-              <Button type="primary" style={{ height: 40, marginTop: 60 }} >高级检索</Button>
+          <Col span={7} offset={0}>
+            <div style={{ marginLeft: 15 }} >
+              <a href={`http://218.206.229.46:8080/projectSearch/index?text=${search_content}`}>
+                <Button type="primary" style={{ height: 40, marginTop: 60 }} >高级检索</Button>
+              </a>
             </div>
           </Col>
         </Row>
 
         <div className="main-1">
           <div className="main-1-top am-flexbox-justify-center">
-            <img src={require("./img/PlatformProfile.png")} />
+            <img alt="alt" src={require("./img/PlatformProfile.png")} />
           </div>
 
           <div className="main-1-footer am-flexbox-dir-row">
@@ -97,7 +107,7 @@ class App extends Component {
               let bool = data.isMove || data.isSelect;
               let className1 = bool ? "main1items" : '';
               return <div key={index} className={`main1item am-flexbox-dir-column ${className1}`} onMouseEnter={() => this.onMouse1(index, true)} onMouseLeave={() => this.onMouse1(index, false)} onClick={() => this.onDown1(index)} >
-                <img src={require(`./img/1${index}.png`)} style={{ height: bool ? 0 : 90 }} />
+                <img alt="alt" src={require(`./img/1${index}.png`)} style={{ height: bool ? 0 : 90 }} />
                 <div dangerouslySetInnerHTML={{ __html: data.name }} style={{ fontSize: 24, paddingTop: 20, paddingLeft: 20, paddingRight: 10 }}></div>
                 <div dangerouslySetInnerHTML={{ __html: data.content }} style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20, paddingRight: 10, height: bool ? 320 : 210 }}></div>
                 <div className='am-flexbox-align-end'>
@@ -111,13 +121,13 @@ class App extends Component {
 
         <div className="main-f">
           <div className="main-f-top am-flexbox-justify-center">
-            <img src={require("./img/serve.png")} />
+            <img alt="alt" src={require("./img/serve.png")} />
           </div>
           <div className="main-f-f">
             <ul className="item1">
               {this.state.mainList2.map((data, index) => (
-                <li key={index} onMouseEnter={() => this.onMouse2(index, true)} onMouseLeave={() => this.onMouse2(index, false)} style={{ marginLeft: index == 0 ? 0 : 35 }}>
-                  <img src={require(`./img/2${index}.png`)} width="265" height="370" />
+                <li key={index} onMouseEnter={() => this.onMouse2(index, true)} onMouseLeave={() => this.onMouse2(index, false)} style={{ marginLeft: index === "0" ? 0 : 35 }}>
+                  <img alt="alt" src={require(`./img/2${index}.png`)} width="265" height="370" />
                   <div className="item1-div" style={{
                     width: 265,
                     height: data.isMove ? 370 : 50,
@@ -140,10 +150,10 @@ class App extends Component {
         <div className="footer">
           <div className="footer-top">
             <div className="footer-a" style={{ color: "#fff" }} >
-              首页 | 关于我们 | 联系我们 | 网站地图 | 产品与服务<br />
+              <a href=''> 首页 </a> | 关于我们 | 联系我们 | 网站地图 | 产品与服务<br />
               电话:0371-1234567  服务信箱:123456789.com<br />
-              xxxxxxxxxxxxxxxxx Copyright 2018 NoiseChina.com Inc All Rights Reserved.
-			</div>
+              xxxxxxxxxxxxxxxxx Copyright 2018 www.zhigengwang.com.cn Inc All Rights Reserved.
+            </div>
           </div>
         </div>
 
@@ -167,12 +177,12 @@ class App extends Component {
     let mainList1 = this.state.mainList1;
     for (let i = 0; i < mainList1.length; i++) {
       const element = mainList1[i];
-      element.isSelect = i == index;
+      element.isSelect = i === index;
     }
     this.setState(mainList1);
   }
 
 
 }
-
-export default App;
+const FormApp = Form.create()(App);
+export default FormApp;
